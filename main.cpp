d0103fd7c87b752ef7b25c1fb82a7567f285c5c8
Arkadiy Pozharskiy <arkady.pozharsky@vicuesoft.com>
2022-10-09 18:46:14 +0400

    some experiments with opencv
    Signed-off-by: Arkadiy Pozharskiy <arkady.pozharsky@vicuesoft.com>

--- main.cpp
+++ main.cpp
@@ -10,6 +10,25 @@
 
 using namespace cv;
 
+inline std::string ToBinary(int n)
+{
+    std::string r;
+    while(n != 0) { r += ( n%2 == 0 ? "0":"1" ); n /= 2;}
+    return r;
+}
+
+std::string BinaryToEightSymbols(std::string binary)
+{
+    int l = binary.length();
+    if (l < 8)
+    {
+        for (int i {}; i < 8 - l; i++)
+            binary.insert(0, "0");
+    }
+    return binary;
+}
+
+
 template<typename ... T>
 inline std::string ToString(T ... rest)
 {
@@ -25,17 +44,34 @@ inline R ErrorExit(R result, T ... rest)
     return result;
 }
 
+inline void StdoutMatr(Mat m) { std::cout << "Matrix = \n" << " " << m << "\n\n"; }
 
 int main(int arg_c, const char** arg_v) {
 
 //    if (arg_c == 1)
 //        return ErrorExit(0, USAGE);
-    int width {500};
-    int height {500};
-    Mat image = Mat::ones(width, height, CV_8U);
-    imshow("test", image);
-    waitKey(0);
-    //std::cout << "image = " << std::endl << " " << image << std::endl << std::endl;
+
+    int width {10};
+    int height {10};
+
+    // Create image matrix (OpenCV + 8byte + Unsigned = CV_8U)
+    Mat image   = Mat::zeros(width, height, CV_8U);
+    // Create initial matrix for CA
+    Mat initial = Mat::zeros(1,height, CV_8U);
+    initial.at<uchar>(0, (int)height / 2 - 1) = 1;
+    // Wolfram rule
+    std::string WfRule = "10110110";
+
+
+    //image.push_back(mat);
+
+    StdoutMatr(initial);
+
+//    for (int i {}; i < image.rows; i++)
+//        for (int j {}; j < image.cols; j++)
+//            if (i == j) image.at<uchar>(i,j) = 255;
+//    imshow("test", image);
+//    waitKey(0);
 
     return 0;
 }
